fde57fed8e3e5bab9bb1465afb473cd0dee158cf
Oskar Rutqvist <oskarru@ed3354-07.ed3354.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2016-02-29 14:22:19 +0100

    almost there at teh msg passing. however still crashes when trying to pass the message to a client.

--- cchat/channel.erl
+++ cchat/channel.erl
@@ -6,33 +6,44 @@
 initial_state(ChannelName) ->
     #channel_st{name = list_to_atom(ChannelName)}.
 
-
+% Joining a the channel.
 handle(St, {join, User}) ->
 	NewState = St#channel_st{users = [User | St#channel_st.users]},
 	{reply, ok, NewState};
 
+% Leaving this channel.
 handle(St, {leave, User}) ->
 	NewState = St#channel_st{users = lists:delete(User, St#channel_st.users)},
 	{reply, ok, NewState};
 
 % Sending messages
-handle(St, {msg, Msg}) ->
+handle(St, {msg, Msg, {sender, user}}) ->  %user@{senderNick, clientPid}
 
 	%ChannelAtom = list_to_atom(Channel),
 	%genserver:request(St#client_st.server, {msg, user(St), Msg}),
-	sendToUsers(St#channel_st.users, Msg),
+
+	
+	SendToAllOfThese = lists:delete(user, St#channel_st.users),
+	NewState = St#channel_st{users = SendToAllOfThese},
+	sendToUsers(St, SendToAllOfThese, sender, Msg),
     {reply, ok, St}.
     %{reply, {error, not_implemented, "Not implemented"}, St} ;
 
 
+% The actual sending of the message via the server.
+send(St, Receiver, Sender, Msg) ->
+	%% Incoming message in client wants --> (someState, {incoming_msg, Channel, Name, Msg}
+	IncomingAtom = {incoming_msg, St#channel_st.name, Sender, Msg},
+	IncomingAtom2 = {incoming_msg, self(), Sender, Msg},
+	genserver:request(Receiver, IncomingAtom).
+	
+	%genserver:request(Receiver, Msg).
 
-send(Receiver, Msg) ->
-	genserver:request(Receiver, Msg).
-
-sendToUsers([], _) ->
+% Recursive function to send to every member of this channel (client).
+sendToUsers(_ ,[], _, _) ->
   ok;
 
-sendToUsers([ H | T ], Msg) ->
-  spawn(fun() -> send(H, Msg) end),
-  sendToUsers(T, Msg).
+sendToUsers(St, [ H | T ], Sender, Msg) ->
+  spawn(fun() -> send(St, H, Sender, Msg) end),
+  sendToUsers(St, T, Sender, Msg).
 
